765b1ec63e402231ada342ec74523be9dd94ff7c
Grace Chiamaka <obieri.grace@gmail.com>
2019-11-04 15:43:14 +0100

    Add base project layout and setup

---
+++ src/Routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import history from '../common/History/History';
+
+import HomePage from '../components/Home/Home';
+
+export default () => (
+  <Router history={history}>
+    <div>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+      </Switch>
+    </div>
+  </Router>
+);
