a9ee5f076aa0e3d22f52cbbbbd0c948dec1c07eb
Grace Chiamaka <obieri.grace@gmail.com>
2019-11-04 18:44:40 +0100

    Add eslint setup and rules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,14 @@ import Aux from '../../common/Hoc/hoc';
 import Header from '../../common/Navigation/Header/Header';
 import Footer from '../../common/Navigation/Footer/Footer';
 import './Home.scss';
-const Home = () => {
-  return (
-    <Aux className="">
-      <Header />
-      <main>
-        <h1>Welcome Home</h1>
-      </main>
-      <Footer />
-    </Aux>
-  )
-}
+
+const Home = () => (
+  <Aux className="">
+    <Header />
+    <main>
+      <h1>Welcome Home</h1>
+    </main>
+    <Footer />
+  </Aux>
+);
 export default Home;
